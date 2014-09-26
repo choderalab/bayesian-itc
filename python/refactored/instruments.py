0ce9ef4cd08e8aa97dde7624a954257f938fa814
Bas Rustenburg <bas.rustenburg@choderalab.org>
2014-09-26 16:01:30 -0400

    Refactoring in progress: extract instruments and experiment classes. - Experiment and Injection class have been placed in experiments.py - Instrument and VP-ITC class have been extracted and simtk.unit has taken the place of Units.py

---
+++ python/refactored/instruments.py
@@ -0,0 +1,34 @@
+"""Contains Isothermal titration calorimeter instrument classes."""
+
+#=========================================================================
+# Isothermal titration calorimeter instrument class.
+#=========================================================================
+from simtk import unit
+
+
+class Instrument(object):
+
+    """
+    An isothermal titration calorimeter instrument.
+    An instrument object consists of several types of data:
+
+    * the manufacturer and model of the instrument
+    * properites of the instrument (sample cell volume)
+    """
+
+    def __init__(self):
+        self.V0 = None  # volume of calorimeter sample cell
+        pass
+
+
+class VPITC(Instrument):
+
+    """
+    The MicroCal VP-ITC.
+    """
+
+    def __init__(self):
+        self.V0 = 1.4301 * unit.milliliter  # volume of calorimeter sample cell
+        # Tellinghuisen volume correction for VP-ITC
+        self.V0 = self.V0 - 0.044 * unit.milliliter
+        return
