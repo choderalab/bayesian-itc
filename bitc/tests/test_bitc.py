35c17c7368bdb67a9ce26b5d6e626b90a133bdfe
Bas Rustenburg <bas.rustenburg@choderalab.org>
2015-02-03 13:59:28 -0500

    Mark unimplemented tests as expected failures.

--- bitc/tests/test_bitc.py
+++ bitc/tests/test_bitc.py
@@ -1,7 +1,6 @@
 import unittest
 
 
-
 class TestImports(unittest.TestCase):
     """Test if all submodules import properly.
     """
--- bitc/tests/test_parser.py
+++ bitc/tests/test_parser.py
@@ -1,6 +1,7 @@
 import unittest
 
 class TestOptparser(unittest.TestCase):
+    @unittest.expectedFailure
     def test_optparser(self):
         # self.assertEqual(expected, optparser(argv))
         assert False # TODO: implement your test here
