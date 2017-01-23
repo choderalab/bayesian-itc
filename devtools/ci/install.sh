e713d735970cd94f0b2f5ef11f0ea8bced58e979
Bas Rustenburg <bas.rustenburg@choderalab.org>
2017-01-23 11:16:18 -0500

    Introduce common build fixes
    Why:
    
    * The builds have broken over time due to changes in conda,
    and the conda servers. This adds a few fixes that should
    get the builds going again.
    
    This change addresses the need by:
    
    * Update the build structure in .travis.yml
    * Update the install script.

--- devtools/ci/install.sh
+++ devtools/ci/install.sh
@@ -1,18 +1,21 @@
-#!/bin/bash
-MINICONDA=Miniconda2-latest-Linux-x86_64.sh
-MINICONDA_MD5=$(curl -s http://repo.continuum.io/miniconda/ | grep -A3 ${MINICONDA} | sed -n '4p' | sed -n 's/ *<td>\(.*\)<\/td> */\1/p')
-wget http://repo.continuum.io/miniconda/${MINICONDA}
-if [[ ${MINICONDA_MD5} != $(md5sum ${MINICONDA} | cut -d ' ' -f 1) ]]; then
+# Temporarily change directory to $HOME to install software
+pushd .
+cd $HOME
+
+# Install Miniconda
+MINICONDA=Miniconda3-latest-Linux-x86_64.sh
+MINICONDA_HOME=$HOME/miniconda3
+MINICONDA_MD5=$(curl -s https://repo.continuum.io/miniconda/ | grep -A3 $MINICONDA | sed -n '4p' | sed -n 's/ *<td>\(.*\)<\/td> */\1/p')
+wget -q http://repo.continuum.io/miniconda/$MINICONDA
+if [[ $MINICONDA_MD5 != $(md5sum $MINICONDA | cut -d ' ' -f 1) ]]; then
     echo "Miniconda MD5 mismatch"
     exit 1
 fi
-bash ${MINICONDA} -b
-PIP_ARGS="-U"
+bash $MINICONDA -b -p $MINICONDA_HOME
 
-export PATH=$HOME/miniconda2/bin:$PATH
+export PATH=$HOME/miniconda3/bin:$PATH
 
-sudo apt-get update
-
+# Configure miniconda
+export PIP_ARGS="-U"
+export PATH=$MINICONDA_HOME/bin:$PATH
 conda update --yes conda
-conda config --add channels http://conda.binstar.org/omnia
-conda install --yes --file devtools/ci/requirements.txt
